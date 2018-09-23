2e7b23b5edc18aa4522c592cdb59b1a2d0c23834
corn1ng <wangkangning1@foxmail.com>
2018-09-23 22:09:16 +0800

    add fake 遗传算法 通过权重计算的优选算法

--- src/dispatch/dispatch.java
+++ src/dispatch/dispatch.java
@@ -2,25 +2,118 @@ package dispatch;
 
 import pojo.SLA;
 import pojo.Service;
+import strategy.optimiseStrategy;
+import strategy.t1s2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 
 public class dispatch {
     public static void dispatchPV(Integer servicePV, Integer realPV, List<SLA> slaList, List<Service> services)
     {
+
+        List<Service> selectService=new ArrayList<>();
         // 如果提供的多余实际需要的，低负载状态，准入。
         if(realPV<servicePV*0.95)
         {
             Integer surpuls = servicePV-realPV;
             System.out.println("低负载状态，准入请求。当前时间片剩余容量"+surpuls);
-
+            // 选择 t1s2 策略；
+            optimiseStrategy strategy =new t1s2();
+            services =strategy.selectStategy(services);
+            int i =0;
+            for(i=0;surpuls>0;i++)
+            {
+                if(services.get(i).getRealPV()-slaList.get(i).getPageView()>0)
+                {
+                    // 超出SLA规定的使用
+                    surpuls=surpuls-(services.get(i).getRealPV()-slaList.get(i).getPageView());
+                    /*
+                    * 双倍价格计算
+                    * */
+                    services.get(i).setAdmitPV(services.get(i).getRealPV());
+                    services.get(i).setRejectPV(0);
+                }
+                else
+                {
+                    services.get(i).setAdmitPV(services.get(i).getRealPV());
+                    services.get(i).setRejectPV(0);
+                }
+            }
+            for(;i<services.size();i++)
+            {
+                if(services.get(i).getRealPV()-slaList.get(i).getPageView()>0)
+                {
+                    services.get(i).setAdmitPV(slaList.get(i).getPageView());
+                    services.get(i).setRejectPV(services.get(i).getRealPV()-slaList.get(i).getPageView());
+                }
+                services.get(i).setAdmitPV(services.get(i).getRealPV());
+                services.get(i).setRejectPV(0);
+            }
         }
         else
         {
             System.out.println("已超过95%，满载预警");
             if(realPV>servicePV)
             {
-                System.out.println("警告！已超载"+"购买"+servicePV+"实际"+realPV);
+                System.out.println("已超载！"+" 提供"+servicePV+" 实际"+realPV);
+                // 遗传算法start
+                Random random =new Random();
+                List<List<Integer>> result  =new ArrayList<>(20);
+                for(int i=0;i<20;i++)
+                {
+                    List<Integer> list =new ArrayList<>();
+                    for(int j=0;j<services.size();j++)
+                    {
+                        Integer index =random.nextInt(3)+1;
+                        list.add(index);
+                    }
+                    for(int j=0;j<services.size();j++)
+                    {
+                        if(list.get(j)!=1)
+                        {
+                            list.set(j,0);
+                        }
+                    }
+                    result.add(list);
+                }
+                // end
+                // 选20的结果
+
+                List<Integer> bag = null;
+                Integer max =0;
+                // 选择 t1s2 策略；
+                optimiseStrategy strategy =new t1s2();
+                for(int j=0;j<20;j++)
+                {
+                    List<Integer> integers =result.get(j);
+                    List<Service> serviceList =new ArrayList<>();
+                    for(int i=0;i<services.size();i++)
+                    {
+                        if(integers.get(i)==1)
+                        {
+                            serviceList.add(services.get(i));
+                        }
+                    }
+                    Integer res =((t1s2) strategy).calcuScore(services);
+                    if(res>max)
+                    {
+                        max= res;
+                        bag =integers;
+                    }
+
+                }
+                
+                for(int i=0;i<services.size();i++)
+                {
+                    System.out.print(bag.get(i)+" ");
+
+                }
+                System.out.println(" ");
+
+                //selectService =strategy.selectStategy(selectService);
 
             }
         }
